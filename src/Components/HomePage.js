3907bf59f500a7ed96ecf557a10399f226cccbb0
Shemeer <shameerbem@gmail.com>
2023-05-30 22:02:07 +0530

    third commit

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import '../styles/Homepage.css'
 
+function keygenerator(){
+    let str = 'abcdefghijklmnopqrstuvwxyz';
+    let res = '';
+    for (let i = 0; i < 7; i++) {
+      res += str.charAt(Math.random() * 5);
+    }
+    return res;
+}
+
 function HomePage({imageData}) {
   return (
     <div className='grid'>
         {
             imageData.map((obj,idx)=>{
                 return(
-                    <div className='grid-item'>
-                        <img src = {obj.urls.regular} width={"100%"} height={"100%"} alt='image' />
+                    <div key={keygenerator()} className='grid-item'>
+                        <img src = {obj.urls.regular} width={"100%"} height={"100%"} alt={obj.alt_description} />
                     </div>
                 )
             })
--- src/Components/SearchImages.js
+++ src/Components/SearchImages.js
@@ -1,10 +1,25 @@
 import axios from 'axios';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import '../styles/SearchImages.css';
 
 function SearchImages({ setData }) {
     let [searchData, setSearchData] = useState('');
-  
+
+    function resetImages() {
+        axios.get("https://api.unsplash.com/photos", {
+            params: {
+                client_id: process.env.REACT_APP_ACCESS_KEY,
+                per_page: 20
+            }
+        }).then(response => {
+            setData(response.data)
+        })
+            .catch(error => console.log(error))
+
+            setSearchData('');
+
+    }
+
 
     function getImages() {
 
@@ -12,11 +27,12 @@ function SearchImages({ setData }) {
             params: {
                 client_id: process.env.REACT_APP_ACCESS_KEY,
                 query: searchData,
-                per_page:20
+                per_page: 20
             }
-        }).then(response =>{ 
+        }).then(response => {
             console.log(response.data.results)
-            setData(response.data.results)})
+            setData(response.data.results)
+        })
             .catch(error => console.log(error))
 
 
@@ -27,6 +43,7 @@ function SearchImages({ setData }) {
             <input className='inputArea'
                 onChange={e => setSearchData(e.target.value)} type='text' value={searchData} placeholder='Search for images...' />
             <button className='btn' onClick={getImages}>Search</button>
+            <button className='btn' onClick={resetImages}>Reset</button>
         </div>
     )
 }
